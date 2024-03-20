a7206bd47473f4ec075b296116b8f4ba6a2a9c28
RanjiVSB <ranjithkumar.e_cse2019@svce.edu.in>
2024-03-20 11:55:41 -0500

    Addind frontend and backend files to my repository

---
+++ Frontend_and_Backend_Files/Backend_Files/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import home, login, validate_login, register, edituser, updateuser, assistant, video_feed, healthreference, nearbydoctors
+from  django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('', login, name="login"),
+    path('validate_login', validate_login, name="validate_login"),
+    path('register', register, name="register"),
+    path('home', home, name="home"),
+    path('edituser/<int:id>', edituser, name="edituser"),
+    path('updateuser', updateuser, name="updateuser"),
+    path('logout/', LogoutView.as_view(), name="logout"),
+     path('assistant', assistant, name="assistant"),
+     path('video_feed/', video_feed, name='video_feed'),
+     path('healthreference/', healthreference, name='healthreference'),
+      path('nearbydoctors/', nearbydoctors, name='nearbydoctors')
+
+]
