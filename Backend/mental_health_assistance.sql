8a276e23f3665a314bf711c59a879691ded954df
JAMMISETTI LAKSHMI SRAVANI <134979723+JLSravani@users.noreply.github.com>
2024-03-20 22:55:22 -0500

    Update mental_health_assistance.sql

--- Backend/mental_health_assistance.sql
+++ Backend/mental_health_assistance.sql
@@ -6,7 +6,6 @@
 -- Generation Time: Mar 20, 2024 at 07:05 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
