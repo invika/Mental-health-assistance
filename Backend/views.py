e667e0eaf71336a75940c39717cac88a9b2182e1
meghanareddy08 <152237078+meghanareddy08@users.noreply.github.com>
2024-04-01 22:48:44 -0500

    Update changes in view video capture

--- Backend/views.py
+++ Backend/views.py
@@ -165,51 +165,50 @@ def extract_features(image):
     feature = np.array(image)
     feature = feature.reshape(1,48,48,1)
     return feature/255.0
-
-@gzip.gzip_page
-def video_feed(request):
+    
+def generate():
     json_file = open("myapp/assitantmodal/facialemotionmodel.json", "r")
     model_json = json_file.read()
     json_file.close()
     model = model_from_json(model_json)
 
     model.load_weights("myapp/assitantmodal/facialemotionmodel.h5")
-    haar_file=cv2.data.haarcascades + 'myapp/assitantmodal/haarcascade_frontalface_default.xml'
+    haar_file=cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
     face_cascade=cv2.CascadeClassifier(haar_file)
 
 
     # Set up camera
     webcam = cv2.VideoCapture(0)
-
     labels = {0 : 'angry', 1 : 'disgust', 2 : 'fear', 3 : 'happy', 4 : 'neutral', 5 : 'sad', 6 : 'surprise'}
 
-   
-    def generate():
-        while True:
-            i,im=webcam.read()
-            gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-            faces=face_cascade.detectMultiScale(im,1.3,5)
-            try: 
-                for (p,q,r,s) in faces:
-                    image = gray[q:q+s,p:p+r]
-                    cv2.rectangle(im,(p,q),(p+r,q+s),(255,0,0),2)
-                    image = cv2.resize(image,(48,48))
-                    img = extract_features(image)
-                    pred = model.predict(img)
-                    prediction_label = labels[pred.argmax()]
-                    # print("Predicted Output:", prediction_label)
-                    # cv2.putText(im,prediction_label)
-                    cv2.putText(im, '% s' %(prediction_label), (p-10, q-10),cv2.FONT_HERSHEY_COMPLEX_SMALL,2, (0,0,255))
-                #cv2.imshow("Output",im)
-                # Convert frame to JPEG
-                ret, buffer = cv2.imencode('.jpg', im)
-                frame = buffer.tobytes()
-                yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    while True:
+        i,im=webcam.read()
+        gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+        faces=face_cascade.detectMultiScale(im,1.3,5)
+        try: 
+            for (p,q,r,s) in faces:
+                image = gray[q:q+s,p:p+r]
+                cv2.rectangle(im,(p,q),(p+r,q+s),(255,0,0),2)
+                image = cv2.resize(image,(48,48))
+                img = extract_features(image)
+                pred = model.predict(img)
+                prediction_label = labels[pred.argmax()]
+                # print("Predicted Output:", prediction_label)
+                # cv2.putText(im,prediction_label)
+                cv2.putText(im, '% s' %(prediction_label), (p-10, q-10),cv2.FONT_HERSHEY_COMPLEX_SMALL,2, (0,0,255))
+            #cv2.imshow("Output",im)
+            # Convert frame to JPEG
+            ret, buffer = cv2.imencode('.jpg', im)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-                cv2.waitKey(27)
-            except cv2.error:
-                pass
+            cv2.waitKey(27)
+        except cv2.error:
+            pass
+
+@gzip.gzip_page
+def video_feed(request):
     return StreamingHttpResponse(generate(), content_type='multipart/x-mixed-replace; boundary=frame')
 
 
