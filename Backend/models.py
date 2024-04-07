d3def9f920b9c6fc9fef6aeb3a148bfe628b4aa4
JAMMISETTI LAKSHMI SRAVANI <134979723+JLSravani@users.noreply.github.com>
2024-04-06 21:17:19 -0500

    Update models.py

--- Backend/models.py
+++ Backend/models.py
@@ -2,6 +2,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+
 # Create your models here.
 class CustomUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
