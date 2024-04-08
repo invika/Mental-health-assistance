ae1e507f004e1e18ba83096cddbd708c7f84f04d
invika <77779844+invika@users.noreply.github.com>
2024-04-07 23:30:42 -0500

    Linking CustomUser to the built-in User model

--- Frontend_and_Backend_Files/Backend_Files/models.py
+++ Frontend_and_Backend_Files/Backend_Files/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+  # Linking CustomUser to the built-in User model
 class CustomUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=100)
