6f51b9771d88ee62a16d391f79a2d437ffb691ae
meghanareddy08 <152237078+meghanareddy08@users.noreply.github.com>
2024-04-11 10:57:55 -0500

    Video capture update and displaying of label

--- myapp/assitantmodal/realtimedetection.py
+++ myapp/assitantmodal/realtimedetection.py
@@ -14,7 +14,7 @@ def extract_features(image):
     feature = np.array(image)
     feature = feature.reshape(1,48,48,1)
     return feature/255.0
-
+#Video captured and label is displayed based on detection
 webcam=cv2.VideoCapture(0)
 labels = {0 : 'angry', 1 : 'disgust', 2 : 'fear', 3 : 'happy', 4 : 'neutral', 5 : 'sad', 6 : 'surprise'}
 while True:
