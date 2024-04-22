2bf7964f81502f9d586eac9d233078fd50a4b350
meghanareddy08 <152237078+meghanareddy08@users.noreply.github.com>
2024-04-22 13:22:34 -0500

    CV2 module update on webcam turning

--- myapp/assitantmodal/realtimedetection.py
+++ myapp/assitantmodal/realtimedetection.py
@@ -15,7 +15,7 @@ def extract_features(image):
     feature = np.array(image)
     feature = feature.reshape(1,48,48,1)
     return feature/255.0
-
+#webcam access for emotion recognition using cv2 module
 webcam=cv2.VideoCapture(0)
 labels = {0 : 'angry', 1 : 'disgust', 2 : 'fear', 3 : 'happy', 4 : 'neutral', 5 : 'sad', 6 : 'surprise'}
 while True:
